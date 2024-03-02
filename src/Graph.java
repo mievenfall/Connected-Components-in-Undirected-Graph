a91b853636b501d4c29008dfdf47320773f931fc
mievenfall <mngoc2603@gmail.com>
2024-03-01 19:28:51 -0800

    add comments

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,12 @@
-import java.util.*;
+/***************************************************************/
+/* Evelyn Vu                                                   */
+/* Login ID: evu                                               */
+/* CS 3310, Spring 2024                                        */
+/* Programming Assignment 1                                    */
+/* Graph class: functions to contruct a graph                  */
+/***************************************************************/
 
+import java.util.*;
 
 class Graph {
 	private int numVertices;;
@@ -7,24 +14,33 @@ class Graph {
 	private boolean[] visited;
 	private List<Set<Integer>> components;
 
-	// constructor
+	// Constructor
 	Graph(int numVertices)
 	{
 		this.numVertices = numVertices;
-		// define the size of array as
-		// number of vertices
 		this.graph = new HashMap<Integer, List<Integer>>();
 		this.visited = new boolean[numVertices+1];
 		this.components = new ArrayList<Set<Integer>>();
 	}
 
-	void addVertex() {
+    /**************************************************************/
+    /* Method: addVertex                                          */
+    /* Purpose: Add all vertices to the graph                     */
+    /**************************************************************/
+	
+    void addVertex() {
         for (int i = 1; i < numVertices+1; i++) {
             graph.put(i, new ArrayList<>());
         }
     }
 
-	// Adds an edge to an undirected graph
+	/**************************************************************/
+    /* Method: addEdge                                            */
+    /* Purpose: Add all vertices to the graph                     */
+    /* Parameters:                                                */
+    /* String[] edges: List of edges given from the input file    */
+    /**************************************************************/
+    
 	void addEdge(String[] edges) {
         for(int i = 1; i < edges.length; i++) {
             String[] edge = edges[i].substring(1, edges[i].length()-1).split(",");
@@ -41,28 +57,70 @@ class Graph {
         }
     }
 
+    /**************************************************************/
+    /* Method: connectComponent                                   */
+    /* Purpose: Using depth-first search to connect vertices that */
+    /*          linked with an edge to create connect components  */
+    /**************************************************************/
+
 	void connectComponent() {
         for(int i = 1; i <= numVertices; i++) {
+
+            // If vertex i is not visited yet, 
+            // create a new set of component
             if(!visited[i]) {
                 Set<Integer> component = new HashSet<Integer>();
+
+                // Perform dfs to add more vertex (if possible)
+                // to create a connected component
                 dfs(graph, i, visited, component);
+
+                // Add the connected component to the list of components
                 components.add(component);
             }
         }
     }
-	void dfs(Map<Integer, List<Integer>> graph, int v, 
+
+    /**************************************************************/
+    /* Method: dfs                                                */
+    /* Purpose: Depth-first search algorithm to create            */
+    /*          connectect components                             */
+    /* Parameters:                                                */
+    /* Map<Integer, List<Integer>> graph: the undirected graph    */
+    /* int vertex: the current vertex of the graph                */
+    /* boolean[] visited: list of visited vertice                 */
+    /* Set<Integer> component: the current component we are       */
+    /*                         trying to connect                  */
+    /**************************************************************/
+
+	void dfs(Map<Integer, List<Integer>> graph, int vertex, 
                            boolean[] visited, Set<Integer> component) {
-        // DFS implementation
-        visited[v] = true;
-        component.add(v);
-        for(int neighbor : graph.get(v)) {
+        
+        // Mark the current vertex as visited
+        visited[vertex] = true;
+        
+        // Add the vertex to the set of component
+        component.add(vertex);
+
+        // Recur for all the neighbor of that vertex
+        for(int neighbor : graph.get(vertex)) {
             if(!visited[neighbor]) {
                 dfs(graph, neighbor, visited, component);
             }
         }
     }
+
+    /**************************************************************/
+    /* Method: printResult                                        */
+    /* Purpose: Print the result in proper format                 */
+    /**************************************************************/
+
 	void printResult() {
+
+        // Initialize StringBuilder to work on building a string
         StringBuilder resultBuilder = new StringBuilder();
+
+        // Format every connected component as {x y z}
         for(Set<Integer> component : components) {
             resultBuilder.append("{");
             boolean prev = true;
@@ -76,9 +134,9 @@ class Graph {
             resultBuilder.append("} ");
         }
 
+        // Finalize string and output to the user
         String result = resultBuilder.toString();
         System.out.println(components.size() + " connected components: " + result);
         System.out.println();
     }
-	// Driver code
 }
--- src/Prog1.java
+++ src/Prog1.java
@@ -3,9 +3,9 @@
 /* Login ID: evu                                               */
 /* CS 3310, Spring 2024                                        */
 /* Programming Assignment 1                                    */
-/* Prompt: For a given undirected graph,                       */
-/*         output the vertices of each connected component     */
-/*******************************************************s********/
+/* Prog1 class: With an undirected graph as input, output      */
+/*              the vertices of each connected component       */
+/***************************************************************/
 
 import java.io.*;
 
@@ -27,22 +27,37 @@ public class Prog1 {
         int graphNum = 1;
         System.out.println();
 
+        // Iterate through every line of input file
         while((line = br.readLine()) != null) {
             System.out.println("Graph" + graphNum + ":  " + line);
             
-            String[] parts = line.split("\s+");
-            int numVertices = Integer.parseInt(parts[0]);
-
-            Graph graph = new Graph(numVertices);
-
-            graph.addVertex();
-            graph.addEdge(parts);
-            graph.connectComponent();
-            graph.printResult();
+            processGraph(line); // Process graph for each line
 
             graphNum++;
         }
 
         br.close();
+    }
+
+    /**************************************************************/
+    /* Method: processGraph                                       */
+    /* Purpose: process graph for each line of the input          */
+    /* Parameters:                                                */
+    /* String line: one line of input file                        */
+    /**************************************************************/
+
+    public static void processGraph(String line) {
+
+        // Split the line to parts to decide the number of vertices and the edges
+        String[] parts = line.split("\s+");
+        int numVertices = Integer.parseInt(parts[0]);
+
+        // Initialize new graph
+        Graph graph = new Graph(numVertices);
+
+        graph.addVertex(); // Add vertices to the new graph
+        graph.addEdge(parts); // Add edges to the new graph
+        graph.connectComponent(); // Create connection between vertices
+        graph.printResult(); // Output
     }
 }
